1a3509d3e09e824664f73c269a94f6d4f23caa50
luciehrbkova <50968340+luciehrbkova@users.noreply.github.com>
2021-01-14 14:45:32 +0000

    default

---
+++ myScript.js
@@ -0,0 +1,24 @@
+
+
+// navigation Menu
+function openNav() 
+{
+    document.getElementById("mySidenav").style.left = "0px";
+}
+/* Set the width of the side navigation to 0 */
+function closeNav() 
+{
+    document.getElementById("mySidenav").style.left = "100vw";
+   
+}
+// card flip
+
+// function flipCard() {
+//   card.classList.toggle('is-flipped')
+// };
+
+
+// var card = document.querySelector('.card');
+// card.addEventListener( 'click', function() {
+//   card.classList.toggle('is-flipped');
+// });
